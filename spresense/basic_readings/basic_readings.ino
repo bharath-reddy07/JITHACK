1ced19d417d0f93c2aac36906fb7a0d7a82cafd6
Shishira <shishiraiyar@gmail.com>
2022-12-17 22:39:01 +0530

    Pothole detection

--- spresense/basic_readings/basic_readings.ino
+++ spresense/basic_readings/basic_readings.ino
@@ -3,6 +3,11 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+#include <math.h>
+
+#define ZSENSITIVITY 3.5
+#define RZSENSITIVITY 0.2
+
 
 Adafruit_MPU6050 mpu;
 
@@ -24,6 +29,10 @@ void setup(void) {
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 }
 
+float prevZ ;
+float prevRZ ;
+
+
 void loop() {
 
   /* Get new sensor events with the readings */
@@ -31,20 +40,39 @@ void loop() {
   mpu.getEvent(&a, &g, &temp);
 
   /* Print out the values */
-  Serial.print(a.acceleration.x);
-  Serial.print(",");
-  Serial.print(a.acceleration.y);
-  Serial.print(",");
-  Serial.print(a.acceleration.z);
-  Serial.print(",");
+//  Serial.print(a.acceleration.x);
+//  Serial.print(",");
+//  Serial.print(a.acceleration.y);
+//  Serial.print(",");
+//  Serial.print(a.acceleration.z);
+//    Serial.print(prevZ);
+//  Serial.print(prevZ - a.acceleration.z);
 
-  Serial.print(g.gyro.x);
-  Serial.print(",");
-  Serial.print(g.gyro.y);
-  Serial.print(",");
-  Serial.print(g.gyro.z);
+
+  if(abs(prevZ - a.acceleration.z) > ZSENSITIVITY){
+    Serial.print("POTHOLEEEE  UP DOWN");
+    delay(1000);
+    }
+
+  if(abs(prevRZ - g.gyro.z) > RZSENSITIVITY){
+    Serial.print("POTHOLEEEE  ROLL ROLL");
+    delay(1000);
+    }
+  //Serial.print(prevRZ - g.gyro.z);
+
+//
+//  Serial.print(g.gyro.x);
+//  Serial.print(",");
+//  Serial.print(g.gyro.y);
+//  Serial.print(",");
+//  Serial.print(g.gyro.z);
+
+  prevZ = a.acceleration.z;
+
+  prevRZ = g.gyro.z;
+  
 
 
   Serial.println("");
-  delay(100);
+  delay(50);
 }
